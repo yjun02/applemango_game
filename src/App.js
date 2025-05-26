c389c49bd5ebd19caf2dcb1bbfa4bb29906b5772
yjun02 <jyjoons@gmail.com>
2025-05-27 01:56:57 +0900

    design fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import MainMenu from './components/MainMenu';
 import GameScreen from './components/GameScreen'; // 다음 단계에서 만들 예정
 import './styles/MainMenu.css';
+import './App.css';
 
 function App() {
     const [isGameStarted, setIsGameStarted] = useState(false);
